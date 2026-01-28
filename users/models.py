b0dfa2db8d913a86e2fc62792358e2c7ed65bac3
Vadim Chepil <vadimvamtf@gmail.com>
2026-01-28 15:31:49 +0200

    Implemented custom User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        # Email is required instead of username
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        # Automatically grant administrator rights
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    # Disable the standard username, use email instead
+    username = None
+    email = models.EmailField(unique=True)
+
+    # Tell Django to use email for authentication
+    USERNAME_FIELD = 'email'
+    # Empty - Django automatically queries USERNAME_FIELD and password
+    REQUIRED_FIELDS = [] 
+
+    objects = UserManager()
