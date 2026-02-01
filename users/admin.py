df2db1e0493741e0efc0dcf0c5bfd9b794159d29
Vadim Chepil <vadimvamtf@gmail.com>
2026-02-01 17:12:45 +0200

    Added user model to Django Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'date_joined', 'last_login', 'is_staff', 'is_active')
+    
+    search_fields = ('email',)
+
+    ordering = ('email', '-date_joined')
+    
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, CustomUserAdmin)
